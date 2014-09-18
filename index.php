18d94a8d6163aeeae04f05d12716f402da7062a1
Delphine <delphine_trahand@yahoo.com>
2014-09-18 12:30:14 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>P3</title>
+    <meta charset="utf-8">
+    <link rel='stylesheet' href='./css/DwaP3.css' type='text/css'>  
+<style>
+</style>
+</head>
+
+<body>
+
+<h1>Delphine Trahand - Dynamic Web Apps Portfolio<br>Project 3</h1>
+
+</body>
+</html>
